1d409798da85b05b944f73c3070094fa685f8e78
Dillon Mulroy <dillon.mulroy@gmail.com>
2017-08-12 11:22:51 -0400

    update boostrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+npm i
 git submodule init
 git submodule update
-git submodule foreach "git checkout develop; git pull; npm i"&
+git submodule foreach "(git checkout develop; git pull; npm i)"
